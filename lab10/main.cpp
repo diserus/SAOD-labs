41abff1013f47c87054b6015792b37252086b7d5
Прозоренко Константин Владиславович <ip314s18@csc.sibsutis.ru>
2024-04-02 12:59:15 +0700

    some changes

--- lab10/main.cpp
+++ lab10/main.cpp
@@ -30,30 +30,7 @@ void FillRand(vector<int> &arr,int n)
     }
 }
 
-// void heapify(vector<int> &arr,int n, int i, int &m, int &c)
-// {
-    
-//     int largest = i;   
-//     int l = 2*i + 1; 
-//     int r = 2*i + 2; 
-    
-//     c+=3;
-
-//     if (l < n && arr[l] > arr[largest])
-//         largest = l;
-
-//     if (r < n && arr[r] > arr[largest])
-//         largest = r;
-
-//     if (largest != i)
-//     {
-//         swap(arr[i], arr[largest]);
-//         m++;
-//         heapify(arr, n, largest,c,m);
-//     }
-// }
-
-int heapify(vector<int> &arr, int i, int R)
+int build_heap(vector<int> &arr, int i, int R)
 {
     
     int m=0;
@@ -62,31 +39,31 @@ int heapify(vector<int> &arr, int i, int R)
     int x=arr[i];
     while (true)
     {
-        c+=2;
+        
         m++;
         int j=2*i;
-        if (j>R) break;
-        if (j<R && arr[j+1]<=arr[j]) j++;
-        if(x<=arr[j]) break;
+        if (j>R) {c++; break;}
+        if (j<R && arr[j+1]<=arr[j]) {j++; c++;}
+        if(x<=arr[j]) {c++; break;}
         arr[i]=arr[j];
         i=j;
     }
+    m++;
     arr[i]=x;
     return m+c;
 }
 
-void heapify(vector<int> &arr, int n, int i,int &m, int &c)
+void build_heap(vector<int> &arr, int n, int i,int &m, int &c)
 {
     
     int x = arr[i];
     while (true)
     {
-        c += 2;
         m++;
         int j = 2 * i + 1;
-        if (j >= n) break;
-        if (j + 1 < n && arr[j + 1] <= arr[j]) j++;
-        if (x <= arr[j]) break;
+        if (j >= n) {c++; break;}
+        if (j + 1 < n && arr[j + 1] <= arr[j]) {c++; j++;}
+        if (x <= arr[j]) {c++; break;}
         arr[i] = arr[j];
         i = j;
     }
@@ -101,14 +78,14 @@ int heapSort(vector<int> &arr, int n)
 
     for (int i = n / 2 - 1; i >= 0; i--)
     {
-        heapify(arr, n, i, m, c);
+        build_heap(arr, n, i, m, c);
     }
 
     for (int i = n - 1; i >= 0; i--)
     {
         m+=2;
         swap(arr[0], arr[i]);
-        heapify(arr, i, 0, m, c);
+        build_heap(arr, i, 0, m, c);
     }
     return m + c;
 }
@@ -131,11 +108,11 @@ int main()
         cout << setw(5) << size;
         cout << setw(11)<<(int)(log2(size) + 2)+(int)(2*log2(size));
         FillDec(arr,size);
-        cout <<  setw(11) << heapify(arr,1,arr.size()-1);
+        cout <<  setw(11) << build_heap(arr,1,arr.size()-1);
         FillRand(arr,size);
-        cout <<  setw(8) << heapify(arr,1,arr.size()-1);
+        cout <<  setw(8) << build_heap(arr,1,arr.size()-1);
         FillInc(arr,size);
-        cout <<  setw(10) << heapify(arr,1,arr.size()-1) << "\n";
+        cout <<  setw(10) << build_heap(arr,1,arr.size()-1) << "\n";
     }
     cout << "\n\n";
     cout << setw(5) << "N"  << setw(11) << "Dec" << setw(8) <<"Rand" << setw(10) << "Inc\n";
@@ -148,6 +125,8 @@ int main()
         FillRand(arr,size);
         cout <<  setw(8) << heapSort(arr,size);
         FillInc(arr,size);
-        cout <<  setw(10) << heapSort(arr,size) <<"\n";
+        cout <<  setw(10) << heapSort(arr,size);
+        // cout << setw(10) << (int)(2 * size * std::log2(size) +size+ 2 + size * std::log2(size) + 6.5 * size - 4) <<"\n";
     }
+    
 }
