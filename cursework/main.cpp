cde0d6543820d9c96af58edeb782797151938036
diserus <prozorenko24@gmail.com>
2024-10-09 22:14:53 +0700

    part 2

--- cursework/main.cpp
+++ cursework/main.cpp
@@ -15,14 +15,14 @@ struct record
 };
 struct List
 {
-    List *next;
+    List *next = nullptr;
     record data;
 };
 
 struct Queue
 {
-    List *head;
-    List *tail;
+    List *head = nullptr;
+    List *tail = nullptr;
     void add(record *rec)
     {
         List *newNode = new List;
@@ -45,6 +45,16 @@ struct Queue
         {
             cout << head->data.a << " " << head->data.b << " " << head->data.c << "\t"
                  << head->data.d << "\t" << head->data.e << endl;
+            head = head->next;
+        }
+    }
+    void clear()
+    {
+        while (head != nullptr)
+        {
+            List *temp = head;
+            head = head->next;
+            delete temp;
         }
     }
 };
@@ -95,34 +105,51 @@ void QuickSort(record *arr[], int L, int R)
     if (i < R)
         QuickSort(arr, i, R);
 }
-int binarySearch(int A[], int n, int elem)
+int parseYear(char *date)
+{
+    int year;
+    int day;
+    int month;
+    std::sscanf(date, "%2d-%2d-%2d", &day, &month, &year);
+    return year;
+}
+int binarySearch(record *A[], int n, int elem, Queue *queue)
 {
     int l = 0;
     int r = n - 1;
-    int i = 0;
-    while (l < r)
+    while (l <= r)
     {
-        i = (l + r) / 2;
-        if (A[i] < elem)
+        int i = (l + r) / 2;
+        int year = parseYear(A[i]->e);
+
+        if (year == elem)
+        {
+            queue->add(A[i]);
+
+            int left = i - 1;
+            while (left >= 0 && parseYear(A[left]->e) == elem)
+            {
+                queue->add(A[left]);
+                left--;
+            }
+            int right = i + 1;
+            while (right < n && parseYear(A[right]->e) == elem)
+            {
+                queue->add(A[right]);
+                right++;
+            }
+            return 0;
+        }
+
+        if (year < elem)
             l = i + 1;
         else
-            r = i;
+            r = i - 1;
     }
-    if (A[r] == elem)
-        return r;
-    else
-        return -1;
+    return 1;
 }
-int main()
+void displayRecords(record *indexArr[])
 {
-    FILE *fp = fopen("testBase4.dat", "rb");
-    record mas3[4000] = {0};
-
-    int readCount = fread((record *)mas3, sizeof(record), 4000, fp);
-    record *indexArr[readCount];
-    for (int i = 0; i < readCount; i++)
-        indexArr[i] = &mas3[i];
-    QuickSort(indexArr, 0, 3999);
     int i = 0;
     while (i < 4000)
     {
@@ -149,5 +176,46 @@ int main()
         }
         i++;
     }
+}
+void displaySearch(record *indexArr[], int readCount)
+{
+    int element;
+    std::cout << "Input records what you want to find(key = year): ";
+    std::cin >> element;
+    Queue *queue = new Queue;
+    if (binarySearch(indexArr, readCount, element, queue))
+        std::cout << "Records didn't find\n";
+    queue->print();
+    queue->clear();
+}
+int main()
+{
+    FILE *fp = fopen("testBase4.dat", "rb");
+    record mas3[4000] = {0};
+
+    int readCount = fread((record *)mas3, sizeof(record), 4000, fp);
+    record *indexArr[readCount];
+    for (int i = 0; i < readCount; i++)
+        indexArr[i] = &mas3[i];
+    QuickSort(indexArr, 0, 3999);
+    char choice;
+    do
+    {
+        cout << "\n1. All records\n2. Search\n3. Exit\n";
+        cin >> choice;
+        if (choice == '1')
+        {
+            displayRecords(indexArr);
+        }
+        else if (choice == '2')
+        {
+            displaySearch(indexArr, readCount);
+        }
+        else if (choice != '1' && choice != '2' && choice != '3')
+        {
+            cout << "Incorrect input" << endl;
+        }
+    } while (choice != '3');
+
     return 0;
 }
